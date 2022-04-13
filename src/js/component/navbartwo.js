62403433adf103c6466a37c7fec0638236e8d2af
lucciii33 <angelomaiele@gmail.com>
2022-04-13 21:56:24 +0000

    navbar2 fixed

--- src/js/component/navbartwo.js
+++ src/js/component/navbartwo.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const NavbarTwo = () => {
 	return (
 		<div className="navbartwo">
-			<p className="text-center">All purchases over 60$ have free shipping in the Usa and Venezuela</p>
+			<p className="text-center m-0">All purchases over 60$ have free shipping in the Usa and Venezuela</p>
 		</div>
 	);
 };
