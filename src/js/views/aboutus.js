b1a0f20847842eeb58a9a9ecf6befe657b82dad9
lucciii33 <angelomaiele@gmail.com>
2022-04-22 19:26:25 +0000

    abputout

--- src/js/views/aboutus.js
+++ src/js/views/aboutus.js
@@ -5,25 +5,23 @@ import { Context } from "../store/appContext";
 import { Card } from "../component/card";
 import shop from "../../img/shop1.webp";
 import F1Car from "../../img/prueba.jpeg";
+import about from "../../img/about.jpg"
 
 export const AboutUs = props => {
 	const { store, actions } = useContext(Context);
 	const params = useParams();
 	return (
-		<div className="">
-			<div>
-				<div className="d-flex justify-content-start">
-				<img src={shop} className="image" style={{width: '550px', heigth: 'auto'}}></img>
-				<p className="text-white">somos una compania especializada en ropa sobre la formula 1</p>
+		<div className="position-relative">
+			<img src={about} style={{width: "100%", height: "100vh"}}>
+			</img> 
+				<div className="position-absolute top-50 end-0 translate-middle-y text-center me-2">
+					<h1>Maiele Racing</h1>
+					<h3>have been found by Angelo Maiele in 2022</h3>
+					<h5>Its been 1 year word shipping amazong f1 products and information</h5>
+					<p className="m-4">for all those fans rigth there, also you have a lot information about f1 like races, times, tracks etc</p>
+					<button className="button-24">visit here!</button>
 				</div>
-				<div className="d-flex justify-content-end">
-				<p className="text-white">somos una compania especializada en ropa sobre la formula 1</p>
-				<img src={F1Car} className="image" style={{width: '550px', heigth: 'auto'}}></img>
-				</div>
-			</div>
-			<div>
-
-			</div>
+			
 		</div>
 	);
 };
